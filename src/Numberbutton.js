cc1507e4b5516be9e6bef960316762a6a3a37d34
Goru <abhiabhi9087@gmail.com>
2023-07-12 17:47:27 +0530

    modified username

--- src/Numberbutton.js
+++ src/Numberbutton.js
@@ -1,12 +1,13 @@
-import React from 'react';
+
 import { ACTIONS } from './App';
 
-function Numberbutton({number,dispatch}) {
+function Numberbutton({ number, dispatch }) {
+
   return (
-    
-         <button onClick={()=> dispatch({type:ACTIONS.ADD_DIGIT,payload:{number}})} className="text-[2rem]  border border-black bg-[#60a5fa] hover:bg-[#93c5fd] focus:bg-[#93c5fd]">
-            {number}
-         </button>
+
+    <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { number } })} className="text-[2rem]  border border-black bg-[#60a5fa] hover:bg-[#93c5fd] focus:bg-[#93c5fd]">
+      {number}
+    </button>
 
   );
 }
